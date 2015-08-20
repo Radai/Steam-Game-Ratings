d5955d98df17fc2c421b247936afe8ee96dda04f
Chris Jagusch <cjagusch@plexdev.io>
2015-08-20 14:21:45 -0400

    added sort function to th

--- functions.js
+++ functions.js
@@ -24,14 +24,21 @@ $("#txtID").keyup(function(e){
 		    success: function(data) {
 		    	var div = $("<table>");
 		    	var tabletr = $("<tr>");
-		    	tabletr.append($("<td class='name-header name'>").text("Game Name"));
-		    	tabletr.append($("<td class='ignrating-header ignrating'>").text("IGN Rating"));
+		    	tabletr.append($("<th class='name-header name'>").text("Game Name"));
+		    	tabletr.append($("<th class='ignrating-header ignrating'>").text("IGN Rating"));
 		    	div.append(tabletr);
 
 		    	$(data.response.games).each(function(){
 		    		makeRow(div, this);
 		    	});
 		    	$("#gameList").append(div);
+		    	$('th').click(function(){
+				    var table = $(this).parents('table').eq(0)
+				    var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()))
+				    this.asc = !this.asc
+				    if (!this.asc){rows = rows.reverse()}
+				    for (var i = 0; i < rows.length; i++){table.append(rows[i])}
+				})
 		    },                                                                                                                                                                                       
 		    error: function() { console.log('Uh Oh!'); },
 		    jsonp: 'jsonp'                                                                                                                                                
@@ -69,6 +76,15 @@ $("#txtID").keyup(function(e){
 					}
 				}
 			});
+		}
+		function comparer(index){
+		    return function(a, b) {
+		        var valA = getCellValue(a, index), valB = getCellValue(b, index)
+		        return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.localeCompare(valB)
+		    }
+		}
+		function getCellValue(row, index){ 
+			return $(row).children('td').eq(index).html() 
 		}
 	}
 });
